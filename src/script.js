07bcf7eab974dc0df6cecbc1a4d5fa93a1e692e3
unknown <r.mevius@iplabs.de>
2018-08-11 18:32:09 +0200

    fix framing

--- src/script.js
+++ src/script.js
@@ -18,18 +18,20 @@ function getRandom(array) {
 function drawImage(ctx, img, x, y, w, h, relx, rely, mirrored, angle, frameIndex) {
     if (relx == null) { relx = 0.5; }
     if (rely == null) { rely = 1.0; }
+    if (frameIndex != undefined) {
+        var frameWidth = img.width / img.frameCount;
+    }
     ctx.save();
     ctx.translate(x, y);
-    x = -relx * img.width;
-    y = -rely * img.height;
+    x = -relx * frameWidth || img.width;
+    y = -rely * frameWidth || img.height;
     if (mirrored) {
         ctx.scale(-1, 1);
     }
     if (angle) {
         ctx.rotate(angle);
     }
-    if (frameIndex) {
-        let frameWidth = img.width / img.frameCount;
+    if (frameWidth != undefined) {
         ctx.drawImage(img, frameIndex * frameWidth, 0, frameWidth, img.height, x, y, w || frameWidth, h || img.height);
     } else if (w || h) {
         ctx.drawImage(img, x, y, w, h);
