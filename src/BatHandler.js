ee85d34e8a1f828d1c91e7a5e6c01fe7b772a0c9
unknown <r.mevius@iplabs.de>
2018-08-13 17:14:13 +0200

    add bat

--- src/BatHandler.js
+++ src/BatHandler.js
@@ -1,18 +1,40 @@
 function BatHandler() {
     this.lastCheck = 0;
+    this.minTimeBetweenSpawns = 0.08;
+    this.spawnProbability = .5;
 }
 
 BatHandler.prototype.update = function(delta) {
     let playerPos = state.player.position;
     // console.log(playerPost);
-    if (state.dayTime % 1 <= 0.25 || state.dayTime % 1 >= 0.75) {
-        if (state.dayTime - this.lastCheck > 0.024) {
-            // if (!state.bat) {
+    if (state.bat) {
+        const timeSinceSpawning = state.dayTime - this.spawningTime;
+        let animationProgress = timeSinceSpawning / this.animationDuration;
+        animationProgress = animationProgress > 1 ? 1 : animationProgress;
+        state.bat.position = [
+            this.startPos[0] + (this.endPos[0] - this.startPos[0]) * animationProgress,
+            this.startPos[1] + (this.endPos[1] - this.startPos[1]) * animationProgress,
+        ];
+        if (animationProgress === 1) {
+            state.bat = null;
+        }
+    }
+    if (state.dayTime % 1 <= 0.25 || state.dayTime % 1 >= 0.75 && !state.bat) {
+        if (state.dayTime - this.lastCheck > this.minTimeBetweenSpawns) {
+            if (Math.random() < this.spawnProbability) {
                 this.startPos = [playerPos[0] - 8, Math.floor(playerPos[1] - 6 + Math.random() * 12)];
                 this.endPos = [playerPos[0] + 8, Math.floor(playerPos[1] - 6 + Math.random() * 12)];
-                state.bat = new Bat([this.startPos[0],this.startPos[1]]);
-            // }
-            this.lastCheck = state.dayTime;
+                this.animationDuration = 0.015 + Math.random() * 0.015 
+                // make bat fly in other direction
+                if (Math.random() < .5) {
+                    const swap = this.startPos;
+                    this.startPos = this.endPos;
+                    this.endPos = swap;
+                }
+                state.bat = new Bat([this.startPos[0], this.startPos[1]]);
+                this.spawningTime = state.dayTime;
+                this.lastCheck = state.dayTime;
+            }
         }
     }
 }
